c22b00c9066c11995650717e17426234cd970eb9
pasqualedimaro <pakydm15@gmail.com>
2025-06-09 17:05:18 +0200

    setup dom elements

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// seleziono elementi da html
+const countdown = document.getElementById('countdown');
+const instructions = document.getElementById('instructions');
+const numbersList = document.getElementById('numbers-list');
+const answersForm = document.getElementById('answers-form');
+
+// creo array per numeri casuali
+const randomNumbers = [];
